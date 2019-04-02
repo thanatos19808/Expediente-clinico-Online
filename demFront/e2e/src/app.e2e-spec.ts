d57000b20d4019c30d84858854cbdb09b840e345
Luis Pineda <yisangel@gmail.com>
2019-04-02 17:18:21 +0000

    inicio desarrollo frontend

--- demFront/e2e/src/app.e2e-spec.ts
+++ demFront/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to demFRONT!');
+    expect(page.getTitleText()).toEqual('Welcome to demFront!');
   });
 
   afterEach(async () => {
--- demFront/src/app/app.component.spec.ts
+++ demFront/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'demFRONT'`, () => {
+  it(`should have as title 'demFront'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('demFRONT');
+    expect(app.title).toEqual('demFront');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to demFRONT!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to demFront!');
   });
 });
